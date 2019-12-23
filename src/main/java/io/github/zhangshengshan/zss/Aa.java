1b2f445b867fc983c57f5e23c82ec708205c5c6a
zhangshengshan <zhangshengshan@youzan.com>
2019-12-23 18:53:13 +0800

    first commit
    Multi-line description of commit,
    feel free to be detailed.
    
    [Ticket: X]

---
+++ src/main/java/io/github/zhangshengshan/zss/Aa.java
@@ -0,0 +1,13 @@
+package io.github.zhangshengshan.zss;
+
+public class Aa {
+
+    public static void main(String[] args) {
+        Long a = 1L;
+        Long b = 2L;
+
+        long L = a + b;
+        System.out.println(L);
+
+    }
+}
